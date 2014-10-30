358d2103a1811d9b8481d8f1ed04e6733da746d7
= <=>
2014-10-29 22:24:33 -0430

    Arreglando debug

--- app/routes.php
+++ app/routes.php
@@ -138,6 +138,50 @@ EOT;
 });
 */
 
+/**
+* jTraceEx() - provide a Java style exception trace
+* @param $exception
+* @param $seen      - array passed to recursive calls to accumulate trace lines already seen
+*                     leave as NULL when calling this function
+* @return array of strings, one entry per trace line
+*/
+function jTraceEx($e, $seen=null) {
+    $starter = $seen ? 'Caused by: ' : '';
+    $result = array();
+    if (!$seen) $seen = array();
+    $trace  = $e->getTrace();
+    $prev   = $e->getPrevious();
+    $result[] = sprintf('%s%s: %s', $starter, get_class($e), $e->getMessage());
+    $file = $e->getFile();
+    $line = $e->getLine();
+    while (true) {
+        $current = "$file:$line";
+        if (is_array($seen) && in_array($current, $seen)) {
+            $result[] = sprintf(' ... %d more', count($trace)+1);
+            break;
+        }
+        $result[] = sprintf(' at %s%s%s(%s%s%s)',
+                                    count($trace) && array_key_exists('class', $trace[0]) ? str_replace('\\', '.', $trace[0]['class']) : '',
+                                    count($trace) && array_key_exists('class', $trace[0]) && array_key_exists('function', $trace[0]) ? '.' : '',
+                                    count($trace) && array_key_exists('function', $trace[0]) ? str_replace('\\', '.', $trace[0]['function']) : '(main)',
+                                    $line === null ? $file : basename($file),
+                                    $line === null ? '' : ':',
+                                    $line === null ? '' : $line);
+        if (is_array($seen))
+            $seen[] = "$file:$line";
+        if (!count($trace))
+            break;
+        $file = array_key_exists('file', $trace[0]) ? $trace[0]['file'] : 'Unknown Source';
+        $line = array_key_exists('file', $trace[0]) && array_key_exists('line', $trace[0]) && $trace[0]['line'] ? $trace[0]['line'] : null;
+        array_shift($trace);
+    }
+    $result = join("\n", $result);
+    if ($prev)
+        $result  .= "\n" . jTraceEx($prev, $seen);
+
+    return $result;
+}
+
 App::error(function(Exception $exception)
 {
 
@@ -152,10 +196,11 @@ App::error(function(Exception $exception)
 
 	$data = [
 		'error'  => $exception,
-		'params' => (Session::get('paramNames')) ? array_combine(Session::get('paramNames'), Input::all()) : NULL
+		'params' => (Session::get('paramNames')) ? array_combine(Session::get('paramNames'), Input::all()) : NULL,
+		'trace'  => jTraceEx($exception)
 	];
 
-	#return View::make('emails.errors')->withError($exception);
+	return View::make('emails.errors', $data);
 
 	Mail::send('emails.errors', $data, function($message) {
 		$message->subject("Oficina Virtual - Error - Debug");
