fb66e720c0899da29e13375d5223038215dbe946
Jose Toro <jose@leadbox.ca>
2015-01-15 05:52:40 -0430

    Fix load news

--- app/controllers/NewsController.php
+++ app/controllers/NewsController.php
@@ -4,34 +4,10 @@ class NewsController extends BaseController {
 
     function get_news($id_taxpayer)
     {
-    	$sql = "SELECT * FROM (
-                    SELECT DISTINCT news.id, news.type, news.title, news.message, news.created, news.id_taxpayer, news.id_tax, news.authomatic, news_users_web.read_date, taxpayer.firm_name, tax.tax_account_number
-                    FROM appweb.news
-                    INNER JOIN taxpayer ON taxpayer.id = news.id_taxpayer
-                    LEFT JOIN appweb.tax ON tax.id = news.id_tax
-                    LEFT JOIN appweb.news_users_web ON news_users_web.id_news = news.id AND news_users_web.id_taxpayer = taxpayer.id
-                    WHERE news.id_taxpayer = $id_taxpayer
-                    AND NOW()::date BETWEEN date_from AND CASE WHEN date_to ISNULL THEN '2030-12-31' ELSE date_to END
-                    AND news_users_web.deleted_at ISNULL
-                    AND news.deleted_at ISNULL
-                    AND news.authomatic
+    	$sql = "SELECT * FROM appweb.get_news_taxpayer(?)
+                ORDER BY created DESC";
 
-                    UNION 
-
-                    SELECT news.id, news.type, news.title, news.message, news.created, news.id_taxpayer, news.id_tax, news.authomatic, news_users_web.read_date, '', ''
-                    FROM appweb.news
-                    INNER JOIN appweb.tax ON tax.id_tax_type = ANY(news.id_tax_types)
-                    LEFT JOIN appweb.news_users_web ON news_users_web.id_news = news.id AND news_users_web.id_taxpayer = tax.id_taxpayer
-                    WHERE tax.id_taxpayer = $id_taxpayer
-                    AND NOW()::date BETWEEN date_from AND CASE WHEN date_to ISNULL THEN '2030-12-31' ELSE date_to END
-                    AND news_users_web.deleted_at ISNULL
-                    AND news.deleted_at ISNULL
-                    AND NOT(news.authomatic)
-                ) AS t
-
-                ORDER BY t.created DESC";
-
-		return DB::select($sql);
+		return DB::select($sql, array($id_taxpayer));
     }
 
     function mark($news, $id_taxpayer)
